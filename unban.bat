0606dfb3c3cc5863481449d2bfbb32ddc1bfc3de
Finn <38991189+ZetaxFTW@users.noreply.github.com>
2022-05-21 23:04:17 +0200

    Update unban.bat

--- unban.bat
+++ unban.bat
@@ -1,15 +1,17 @@
 @echo off
-start cmd.exe /c mkdir unban
+start cmd.exe /c mkdir files
 curl https://github.com/sr2echa/Monotone-HWID-Spoofer/archive/refs/tags/0.0.1.zip -L -o spoofer.zip
-powershell -Command "Expand-Archive spoofer.zip -DestinationPath unban"
-start unban\Monotone-HWID-Spoofer-0.0.1\Monotone.exe
+powershell -Command "Expand-Archive spoofer.zip -DestinationPath files"
 del spoofer.zip
-color A
 cd %APPDATA%
 cd ../LocalLow/"Another Axiom"
 rmdir /s /q "Gorilla Tag"
 echo Removing Another Axiom from registry
 reg delete "HKEY_CURRENT_USER\Software\Another Axiom" /f
 REG QUERY "HKEY_CURRENT_USER\Software\Another Axiom"
+cls
 echo Script completed. Please reboot your computer and use a VPN.
 echo Good VPNs are: NordVPN, PrivatVPN, Surfshark. You can use free ones as well, but they are not guaranteed to work.
+color A
+echo Please start Monotone Spoofer now. You can find it under /files/Monotone-HWID-Spoofer-0.0.1/Monotone.exe
+pause
